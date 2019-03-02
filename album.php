2d806a59f1d42d3cca0f0479891e5a88273c6034
DougCaswell <dougcaswell@gmail.com>
2019-03-02 10:49:51 -0700

    working on album page now

---
+++ album.php
@@ -0,0 +1,27 @@
+<?php 
+include('includes/header.php'); 
+
+if(isset($_GET['id'])) {
+    $albumId = $_GET['id'];
+} else {
+    header("location: index.php");
+}
+
+$album = new Album($con, $albumId);
+
+$artist = $album->getArtist();
+?>
+
+<div class='entityInfo'>
+    <div class="leftSection">
+        <img src="<?php echo $album->getAlbumArtwork(); ?>" alt="Album Artwork">
+    </div>
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <p>By <?php echo $artist->getName(); ?></p>
+        <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
--- index.php
+++ index.php
@@ -1,151 +1,24 @@
-<?php 
-include('includes/config.php');
+<?php include('includes/header.php'); ?>
 
-//session_destroy();
+<h1 class='pageHeadingBig'>You Might Also Like</h1>
 
-if(isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-    header('Location: register.php');
-}
+<div class='gridViewContainer'>
 
-?>
+    <?php 
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY rand() LIMIT 10;");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Slotify</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    
-    <div id='mainContainer'>
-
-        <div id='topContainer'>
-
-            <div id='navBarContainer'>
-                <nav class='navBar'>
-
-                    <a href="index.php" class='logo'>
-                        <img src="https://img.icons8.com/material/32/07d159/paper-windmill.png" alt="Logo"> <p>Slotify</p>
+        while($row = mysqli_fetch_array($albumQuery)) {
+            echo "<div class='gridViewItem'>
+                    <a href='album.php?id=" . $row['id'] . "'>
+                        <img src='" . $row['artworkPath'] . "' alt='Album Artwork'>
+                        <div class='gridViewInfo'>" 
+                        . $row['title'] .
+                        "</div>
                     </a>
+                </div>";
+        }
+    ?>
 
-                    <div class="group">
+</div>
 
-                        <div class='navItem'>
-                            <a href="search.php" class='navItemLink'>Search</a>
-                        </div>
-
-                    </div>
-
-                    <div class="group">
-                    <div class='navItem'>
-                            <a href="browse.php" class='navItemLink'>Browse</a>
-                        </div><div class='navItem'>
-                            <a href="yourMusic.php" class='navItemLink'>Your Music</a>
-                        </div><div class='navItem'>
-                            <a href="profile.php" class='navItemLink'>Profile</a>
-                        </div>
-                    </div>
-
-                </nav>
-            </div>
-
-            <div>
-
-            </div>
-
-        </div>
-        
-        <div id='nowPlayingBarContainer'>
-            <div id='nowPlayingBar'>
-                <div id='nowPlayingLeft'>
-                    <div class='content'>
-                        <span class='albumLink'>
-                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/5/54/Public_image_ltd_album_cover.jpg/220px-Public_image_ltd_album_cover.jpg" alt="Album" class='albumArtwork'>
-                        </span>
-    
-                        <div class='trackInfo'>
-    
-                            <span class='trackName'>
-                                <span>Happy Birthday</span>
-                            </span>
-                            <span class='artistName'>
-                                <span>Doug Caswell</span>
-                            </span>
-    
-                        </div>
-    
-                    </div>
-                </div>
-    
-                <div id='nowPlayingCenter'>
-    
-                    <div class='content playerControls'>
-    
-                        <div class='buttons'>
-                            <button class='controlButton shuffle' title='Shuffle'>
-                                <img src='assets/images/icons/shuffle.png' alt="Shuffle">
-                            </button>
-                            <button class='controlButton previous' title='Previous'>
-                                <img src='assets/images/icons/previous.png' alt="Previous">
-                            </button>
-                            <button class='controlButton play' title='Play'>
-                                <img src='assets/images/icons/play.png' alt="Play">
-                            </button>
-                            <button class='controlButton pause' title='Pause' style='display: none;'>
-                                <img src='assets/images/icons/pause.png' alt="Pause">
-                            </button>
-                            <button class='controlButton next' title='Next'>
-                                <img src='assets/images/icons/next.png' alt="Next">
-                            </button>
-                            <button class='controlButton repeat' title='Repeat'>
-                                <img src='assets/images/icons/repeat.png' alt="Repeat">
-                            </button>
-                        </div>
-    
-                        <div class='playbackBar'>
-    
-                            <span class='progressTime current'>0.00</span>
-                            
-                            <div class='progressBar'>
-    
-                                <div class='progressBarBG'>
-                                    <div class='progress'></div>
-                                </div>
-                            
-                            </div>
-                            <span class='progressTime remaining'>0.00</span>
-    
-                        </div>
-    
-                    </div>
-                </div>
-    
-                <div id='nowPlayingRight'>
-                    <div class='volumeBar'>
-                        <button class='controlButton volume' title='Volume'>
-                            <img src="assets/images/icons/volume.png" alt="Volume">
-                        </button>
-    
-                        <div class='progressBar'>
-    
-                            <div class='progressBarBG'>
-                                <div class='progress'></div>
-                            </div>
-                            
-                        </div>
-    
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-</body>
-
-</html>
+<?php include('includes/footer.php'); ?>
